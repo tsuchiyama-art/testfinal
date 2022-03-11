30380324b4f7c5936390799afe52717b167cce44
tsuchiyama-art <101274501+tsuchiyama-art@users.noreply.github.com>
2022-03-12 00:09:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter=require("./routes/hello");
+var applepageRouter=require("./routes/applepage");
+var bullRouter=require("./routes/bull");
+
 
 var app = express();
 
@@ -23,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/hello",helloRouter);
+app.use("/applepage",applepageRouter);
+app.use("/bull",bullRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
